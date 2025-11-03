3cc2a28513f047f2e312e8bb434022abaf4b87ab
cavalostotosos <tomazini.antonio@escola.pr.gov.br>
2025-11-03 19:38:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const my0bserver = new IntersectionObserver( (entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => my0bserver.observe(element))
+
